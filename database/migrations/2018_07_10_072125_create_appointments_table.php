9e4ee4deba1a6cf0a6cf81f3869f7fc960378245
mariapauly <mariapauly40@gmail.com>
2018-07-10 13:00:05 +0530

    sfd

---
+++ database/migrations/2018_07_10_072125_create_appointments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('staff');
+            $table->string('service');
+            $table->string('time');
+            $table->string('status');
+            $table->string('name');
+            $table->string('mobile');
+            $table->string('email');
+            $table->string('address');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
